14804422588060011fe9de9533bfaf03db9c6a9c
T.J. Schuck <tj@getharvest.com>
2014-12-05 20:57:16 -0500

    Add explicit name for the route

--- mounted_route_420b1/config/routes.rb
+++ mounted_route_420b1/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   require '../mountable/web'
-  mount Mountable::Web, at: 'some_route'
+  mount Mountable::Web, at: 'some_route', as: 'named'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
